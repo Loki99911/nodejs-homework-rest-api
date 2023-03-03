016bdcf50cd8bf13ea5c3734da1b294aef76072c
Loki99911 <nikita_kresik@i.ua>
2023-03-03 15:21:18 +0200

    2

--- helpers/sendEmail.js
+++ helpers/sendEmail.js
@@ -1,15 +1,15 @@
 const sgEmail = require("@sendgrid/mail");
 require("dotenv").config();
 
-const { SENDGRID_API_KEY } = process.env;
+const { SENDGRID_API_KEY, SENDGRID_MAIL } = process.env;
 
 sgEmail.setApiKey(SENDGRID_API_KEY);
 
 const sendEmail = async (data) => {
   const email = {
     ...data,
-    // from: "nikita_kresik@i.ua",
-    from: "alenushakova@gmail.com",
+    // from: "",
+    from: SENDGRID_MAIL,
   };
   await sgEmail.send(email);
   return true;
